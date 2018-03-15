fdb7a0491280cf0ce54847f44290d826a1e5473b
Khanna, Bharat <bharat.khanna@team.telstra.com>
2018-03-15 16:43:07 +0530

    INPUT field initial component

---
+++ src/app/components/input/input-clearable.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+
+@Component({
+  selector: 'input-clearable',
+  templateUrl: './input-clearable.component.html',
+  styleUrls: ['./input-clearable.component.scss']
+})
+export class InputComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {}
+}
